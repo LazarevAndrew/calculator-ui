a38d614aa79b8e916fb4705f48285d5fc7910e54
LazarevAndrew <lazarev.js@gmail.com>
2022-10-11 15:44:42 +0200

    Small improvements

--- src/components/molecules/CalculatorHistory.tsx
+++ src/components/molecules/CalculatorHistory.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Box, ListIcon, List, ListItem, Flex, Button } from "@chakra-ui/react";
+import { Box, ListIcon, List, ListItem, Button } from "@chakra-ui/react";
 import { MdCheckCircle, MdError } from 'react-icons/md'
 
 import { CalculationStatus, useAppContext } from "../organisms/Calculator";
@@ -7,16 +7,18 @@ import { CalculationStatus, useAppContext } from "../organisms/Calculator";
 export default function CalculatorHistory() {
   const { historyActive, history, setHistoryActive } = useAppContext();
 
+  if(!history.length) return <></>;
+
   return (
     <Box>
-        {history.length && (<Button
+        <Button
             aria-label="History"
             onClick={() => setHistoryActive(!historyActive)}
             variant="unstyled"
             fontSize="small"
             zIndex="2"
             color="grey"
-        >{historyActive ? 'Hide': 'Show more'} calculations</Button>)}
+        >{historyActive ? 'Show last calculation': 'Show more calculations'}</Button>
         <List spacing={3}>
         {/* retunrs last 5 logged calculations by default state */}
         {history.slice(historyActive ? Number((process.env.HISTORY_COUNT || -5)) : -1).map(({ calc, status }, index) => (
